a76cc1edb831a07db92337d31be7457941f8cd3c
jcorvi <jcorvi@bsclife022.bsc.es>
2018-05-24 17:25:39 +0200

    PubMed Standardization SQLite

---
+++ model.py
@@ -0,0 +1,61 @@
+'''
+Created on May 18, 2018
+
+@author: jcorvi
+'''
+
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, String, Sequence
+
+Base = declarative_base()
+
+class PubMedRetrieval(Base):
+    __tablename__ = 'pubmed_retrieval'
+    id = Column(Integer, Sequence('id'), primary_key=True)
+    filename = Column(String(250))
+    download = Column(Integer)
+    download_datetime = Column(String(250))
+    download_path = Column(String(400))
+    unzip = Column(Integer)
+    unzip_datetime = Column(String(250))
+    unzip_path = Column(String(400))
+    def __repr__(self):
+        return "<PubMedRetrieval(filename='%s', download='%s', download_datetime='%s',download_path='%s', unzip='%s', unzip_datetime='%s', unzip_path='%s')>" % (
+                                self.filename, self.download, self.download_datetime, self.download_path, self.unzip, self.unzip_datetime, self.unzip_path)
+    
+    """Consctructor"""
+    def __init__(self, filename, download,download_datetime,download_path,unzip, unzip_datetime,unzip_path):
+        self.filename = filename
+        self.download = download
+        self.download_datetime = download_datetime
+        self.download_path = download_path
+        self.unzip = unzip
+        self.unzip_datetime = unzip_datetime
+        self.unzip_path = unzip_path
+        
+        
+class PubMedArticle(Base):
+    __tablename__ = 'pubmed_articles'
+    id = Column(Integer, Sequence('id'), primary_key=True)
+    pmid = Column(String(100))
+    filename = Column(String(250))
+    json = Column(Integer)
+    json_datetime = Column(String(250))
+    json_path = Column(String(400))
+    txt = Column(Integer)
+    txt_datetime = Column(String(250))
+    txt_path = Column(String(400))
+    def __repr__(self):
+        return "<PubMedArticle(pmid='%s', filename='%s', download='%s', download_datetime='%s',download_path='%s', txt='%s', txt_datetime='%s', txt_path='%s')>" % (
+                                self.pmid, self.filename, self.json, self.json_datetime, self.json_path, self.txt, self.txt_datetime, self.txt_path)
+    
+    """Consctructor"""
+    def __init__(self,pmid,filename, json,json_datetime,json_path,txt, txt_datetime,txt_path):
+        self.pmid = pmid
+        self.filename = filename
+        self.json = json
+        self.json_datetime = json_datetime
+        self.json_path = json_path
+        self.txt = txt
+        self.txt_datetime = txt_datetime
+        self.txt_path = txt_path
--- pubmed_standardization.py
+++ pubmed_standardization.py
@@ -7,74 +7,78 @@ import gzip
 import xml.etree.ElementTree as ET
 import json
 import xmltodict 
-
+from db_util import InitDataBase
+import ConfigParser
+from dao import DAO
+from model import PubMedRetrieval
+from model import PubMedArticle
 DOCS_FOR_FOLDER=1000
 
 parser=argparse.ArgumentParser()
 parser.add_argument('-o', help='Output Directory')
-parser.add_argument('-r', help='Remove All Before downloading, warning you have to be sure of remove al the PubMed Database')
+parser.add_argument('-u', help='SQLITE Database URL')
+parser.add_argument('-p', help='Path Parameters')
 args=parser.parse_args()
-
+parameters={}
 if __name__ == '__main__':
     import pubmed_standardization
-    try:
-        dest=args.o
-        remove=args.r
-    except Exception as inst:
-        print( "Error: leyendo los parametros.")
-        sys.exit(1) 
-    if dest==None:
-        print( "Error: complete the destination path") 
-        sys.exit(1)    
-    if not os.path.exists(dest):
-        print( "Error: the destination path does not exist.") 
-        sys.exit(1) 
-    pubmed_standardization.Main(args)
-    
+    parameters = pubmed_standardization.ReadParameters(args)     
+    InitDataBase(parameters)
+    pubmed_standardization.Main(parameters)
 
-def Main(args):
-    dest=args.o
-    result_file = dest + "/index.csv"
+def Main(parameters):
+    dest=parameters['output_directory']
     standardization_output = dest + "/standardization/"
     standardization_input = dest + "/retrieval/"
     if not os.path.exists(standardization_output):
         os.makedirs(standardization_output)
-    if os.path.isfile(result_file):
-        df = pandas.read_csv(result_file, header=0, index_col=0)
-    else:
-        print ("The update_histroy file is missing.")    
-        sys.exit(1) 
-    unzip(df,standardization_input,result_file)
-    standardization(df,standardization_input,standardization_output,result_file)
+    unzip(standardization_input)
+    standardization(standardization_input,standardization_output)
 
-def unzip(df,standardization_input,result_file):
-    df_=df.loc[df['retrieval'] == 'complete']
-    for index, data in df_.iterrows():
-        file=os.path.join(standardization_input+"/"+data['folder'], data['name'])
+def ReadParameters(args):
+    if(args.p!=None):
+        Config = ConfigParser.ConfigParser()
+        Config.read(args.p)
+        parameters['database_url']=Config.get('DATABASE', 'url')
+        parameters['output_directory']=Config.get('MAIN', 'output')
+    if(args.u!=None):
+        parameters['database_url']=args.u
+    if(args.o!=None):
+        parameters['output_directory']=args.o
+    return parameters
+
+def unzip(standardization_input):
+    dao = DAO()
+    pubMedRetrievals = dao.findAllForUnzip(PubMedRetrieval)
+    for pubMedRetrieval in pubMedRetrievals:
+        print pubMedRetrieval.filename
+        file=os.path.join(standardization_input+"/"+pubMedRetrieval.download_path, pubMedRetrieval.filename)
         xml_file_path = file + ".xml"
         if os.path.isfile(file):
             with open(xml_file_path,'w') as xml_file:
-                with gzip.open(os.path.join(standardization_input+"/"+data['folder'], data['name']), 'rb') as f:
+                with gzip.open(os.path.join(standardization_input+"/"+pubMedRetrieval.download_path, pubMedRetrieval.filename), 'rb') as f:
                     file_content = f.read()
                     xml_file.write(file_content)
                     xml_file.flush()
                     xml_file.close()
-                    print data['name'] + " unziped"
-                    df.at[index,'unzip']='complete'
-                    df.to_csv(result_file)
+                    print pubMedRetrieval.filename + " unzip"
+                    pubMedRetrieval.unzip=1
+                    pubMedRetrieval.unzip_datetime=datetime.now()
+                    pubMedRetrieval.unzip_path=pubMedRetrieval.download_path
+                    dao.save(pubMedRetrieval)
         else:
             print ("The file " + file + " not exist, please review and download again ")    
-            df.at[index,'unzip']='not_tar_file'
-            df.to_csv(result_file)
+           
                        
-def standardization(df,standardization_input, standardization_output,result_file):
-    df_=df.loc[df['unzip'] == 'complete']
-    for index, data in df_.iterrows():
-        workdir_input = standardization_input+"/"+data['folder']
-        workdir_output = standardization_output+"/"+data['folder']
+def standardization(standardization_input, standardization_output):
+    dao = DAO()
+    pubMedRetrievals = dao.findAllForStandardization(PubMedRetrieval)
+    for pubMedRetrieval in pubMedRetrievals:
+        workdir_input = standardization_input+"/"+pubMedRetrieval.unzip_path
+        workdir_output = standardization_output+"/"+pubMedRetrieval.unzip_path
         if not os.path.exists(workdir_output):
             os.makedirs(workdir_output)
-        file=os.path.join(workdir_input, data['name'])
+        file=os.path.join(workdir_input, pubMedRetrieval.filename)
         xml_file_path = file + ".xml"
         docs_quantity = DOCS_FOR_FOLDER
         internal_folder_q = 0 
@@ -90,6 +94,10 @@ def standardization(df,standardization_input, standardization_output,result_file
                                 os.makedirs(internal_folder)
                         pmid = article.find("MedlineCitation").find("PMID").text
                         print ("Processing pmid:" + pmid)
+                        pubmedArticle = dao.findPubMedArticleByPMID(PubMedArticle,pmid)
+                        if(pubmedArticle==None):
+                            pubmedArticle = PubMedArticle(pmid,"PMID"+pmid,'0','null','null','0','null','null')
+                            dao.save(pubmedArticle) 
                         xml_string = ET.tostring(article, encoding='utf-8', method='xml')
                         o = xmltodict.parse(xml_string, encoding='utf-8')
                         jsonString = json.dumps(o, indent=4)
@@ -97,21 +105,26 @@ def standardization(df,standardization_input, standardization_output,result_file
                         json_file.write(jsonString)
                         json_file.flush()
                         json_file.close()
-                        data['json']='complete'
+                        pubmedArticle.json=1
+                        pubmedArticle.json_path=pubMedRetrieval.unzip_path + "/" + str(internal_folder_q)
+                        pubmedArticle.json_datetime=datetime.now()
+                        dao.save(pubmedArticle) 
                         txt = ET.tostring(article, encoding='utf-8', method='text')
                         txt_file=open(internal_folder+"/PMID"+pmid+".txt",'w')
                         txt_file.write(txt)
                         txt_file.flush()
                         txt_file.close()
-                        data['txt']='complete'
+                        pubmedArticle.txt=1
+                        pubmedArticle.txt_path=pubMedRetrieval.unzip_path + "/" + str(internal_folder_q)
+                        pubmedArticle.txt_datetime=datetime.now()
+                        dao.save(pubmedArticle) 
                         docs_quantity = docs_quantity - 1
                         if(docs_quantity==0):
                             docs_quantity=DOCS_FOR_FOLDER
                     except Exception as inst:
-                        data['txt']='complete'
-                        data['json']='complete'
-                        print "Error Generando el JSON PMID " + pmid
+                        print "Error Generando el JSON/TXT PMID " + pmid
                         print inst
                         x = inst.args
                         print x 
+                        
                                 
