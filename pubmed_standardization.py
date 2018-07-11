d6d4575b867e31144496376739abae60f7ebf876
jcorvi <jcorvi@bsclife022.bsc.es>
2018-07-11 17:42:02 +0200

    changed to xml format

--- pubmed_standardization.py
+++ pubmed_standardization.py
@@ -12,7 +12,7 @@ import ConfigParser
 from dao import DAO
 from model import PubMedRetrieval
 from model import PubMedArticle
-DOCS_FOR_FOLDER=1000
+DOCS_FOR_FOLDER=100
 import codecs
 parser=argparse.ArgumentParser()
 parser.add_argument('-o', help='Output Directory')
@@ -33,7 +33,7 @@ def Main(parameters):
     if not os.path.exists(standardization_output):
         os.makedirs(standardization_output)
     unzip(standardization_input)
-    standardization(standardization_input,standardization_output)
+    standardization2(standardization_input,standardization_output)
 
 def ReadParameters(args):
     parameters_error=False
@@ -90,19 +90,20 @@ def standardization(standardization_input, standardization_output):
     for pubMedRetrieval in pubMedRetrievals:
         workdir_input = standardization_input+"/"+pubMedRetrieval.unzip_path
         workdir_output = standardization_output+"/"+pubMedRetrieval.unzip_path
+        if not os.path.exists(workdir_output):
+            os.makedirs(workdir_output)
         dirs = os.listdir( workdir_output )
-        internal_folder_q = len(dirs) + 1
+        internal_folder_q = len(dirs)
         '''if(old_unzip_path!=workdir_output):
             internal_folder_q = 0
         old_unzip_path = workdir_output''' 
-        if not os.path.exists(workdir_output):
-            os.makedirs(workdir_output)
         file=os.path.join(workdir_input, pubMedRetrieval.filename)
         xml_file_path = file + ".xml" 
         docs_quantity = DOCS_FOR_FOLDER
         if os.path.isfile(xml_file_path):
-        #if os.path.isfile("/home/jcorvi/text_mining_data_test/pubmed_data//retrieval//baseline/pubmed_result.xml"):
-            with open(xml_file_path,'r') as xml_file:
+        #if os.path.isfile("/home/jcorvi/text_mining_data_test/pubmed_data//retrieval//baseline/example_keywords.xml"):
+            #with open("/home/jcorvi/text_mining_data_test/pubmed_data//retrieval//baseline/example_keywords.xml",'r') as xml_file:
+            with open(xml_file_path,'r') as xml_file:    
                 print ("Parsing " + pubMedRetrieval.filename)
                 docXml = ET.parse(xml_file)
                 for article in docXml.findall("PubmedArticle"):
@@ -132,7 +133,31 @@ def standardization(standardization_input, standardization_output):
                                 abstract_text = abstract_xml.find("AbstractText")
                                 if(abstract_text!=None):
                                     abstract=abstract_text.text
-                                    txt_file.write('<ABSTRACT_LIMTOX>.\n'+abstract)
+                                    txt_file.write('<ABSTRACT_LIMTOX>.\n'+abstract+'\n')
+                                    
+                            txt_file.write('<KEYWORDS_LIMTOX>.\n')
+                            chemicalList = article.find("MedlineCitation").find("ChemicalList")        
+                            if(chemicalList!=None):
+                                txt_file.write('<KEYWORDS_MESHCHEMICAL_LIMTOX>.\n')
+                                for chemical in chemicalList.findall("Chemical"):
+                                    name_substance = chemical.find("NameOfSubstance")
+                                    if(name_substance!=None):
+                                        txt_file.write("<NAME>"+name_substance.text+"</NAME>"+"<UI>"+name_substance.get('UI')+"</UI>."+'\n')
+                                    
+                            keyWords = article.find("MedlineCitation").find("KeywordList")        
+                            if(keyWords!=None):
+                                txt_file.write('<KEYWORDS_MANUAL_LIMTOX>.\n')
+                                for keyword in keyWords.findall("Keyword"):
+                                    txt_file.write("<NAME>"+keyword.text+"</NAME>."+'\n')
+                                    
+                            meshs = article.find("MedlineCitation").find("MeshHeadingList")        
+                            if(meshs!=None):
+                                txt_file.write('<KEYWORDS_MESH_LIMTOX>.\n')
+                                for mesh in meshs.findall("MeshHeading"):
+                                    name_mesh = mesh.find("DescriptorName")
+                                    if(name_mesh!=None):
+                                        txt_file.write("<NAME>"+name_mesh.text+"</NAME>."+'\n')
+
                             txt_file.flush()
                             txt_file.close()    
                             pubmedArticle.txt=1
@@ -147,5 +172,102 @@ def standardization(standardization_input, standardization_output):
                         print inst
                         x = inst.args
                         print x 
+
+def standardization2(standardization_input, standardization_output):
+    dao = DAO()
+    old_unzip_path = None
+    pubMedRetrievals = dao.findAllForStandardization(PubMedRetrieval)
+    for pubMedRetrieval in pubMedRetrievals:
+        workdir_input = standardization_input+"/"+pubMedRetrieval.unzip_path
+        workdir_output = standardization_output+"/"+pubMedRetrieval.unzip_path
+        if not os.path.exists(workdir_output):
+            os.makedirs(workdir_output)
+        dirs = os.listdir( workdir_output )
+        internal_folder_q = len(dirs)
+        '''if(old_unzip_path!=workdir_output):
+            internal_folder_q = 0
+        old_unzip_path = workdir_output''' 
+        file=os.path.join(workdir_input, pubMedRetrieval.filename)
+        xml_file_path = file + ".xml" 
+        docs_quantity = DOCS_FOR_FOLDER
+        if os.path.isfile(xml_file_path):
+        #if os.path.isfile("/home/jcorvi/text_mining_data_test/pubmed_data//retrieval//baseline/example_keywords.xml"):
+            #with open("/home/jcorvi/text_mining_data_test/pubmed_data//retrieval//baseline/example_keywords.xml",'r') as xml_file:
+            with open(xml_file_path,'r') as xml_file:    
+                print ("Parsing " + pubMedRetrieval.filename)
+                docXml = ET.parse(xml_file)
+                for article in docXml.findall("PubmedArticle"):
+                    try:
+                        pmid = article.find("MedlineCitation").find("PMID").text
+                        print ("Processing pmid:" + pmid)
+                        
+                        article_xml = article.find("MedlineCitation").find("Article")
+                        abstract_xml = article_xml.find("Abstract")
+                        if(abstract_xml!=None):
+                            pubmedArticle = dao.findPubMedArticleByPMID(PubMedArticle,pmid)
+                            if(pubmedArticle==None):
+                                pubmedArticle = PubMedArticle(pmid,"PMID"+pmid, pubMedRetrieval.filename,'0','null','null','0','null','null')
+                                dao.save(pubmedArticle) 
+                            if(docs_quantity==DOCS_FOR_FOLDER):
+                                internal_folder_q = internal_folder_q + 1
+                                internal_folder = workdir_output + "/" + str(internal_folder_q)
+                                if not os.path.exists(internal_folder):
+                                    os.makedirs(internal_folder)
+                            txt_file=codecs.open(internal_folder+"/PMID"+pmid+".xml",'w',encoding='utf8')
+                            title_xml=article_xml.find("ArticleTitle")
+                            
+                            txt_file.write('<ARTICLE_LIMTOX>\n')
+                            txt_file.write('<SECTIONS_LIMTOX>\n')
+                            if(title_xml!=None):
+                                title = title_xml.text
+                                txt_file.write('<TITLE_LIMTOX>'+title+'</TITLE_LIMTOX>\n')
+                            abstract_xml = article_xml.find("Abstract")
+                            if(abstract_xml!=None):
+                                abstract_text = abstract_xml.find("AbstractText")
+                                if(abstract_text!=None):
+                                    abstract=abstract_text.text
+                                    txt_file.write('<ABSTRACT_LIMTOX>'+abstract+'</ABSTRACT_LIMTOX>\n')
+                            txt_file.write('</SECTIONS_LIMTOX>\n')
+                            txt_file.write('<KEYWORDS_LIMTOX>\n')
+                            chemicalList = article.find("MedlineCitation").find("ChemicalList")        
+                            if(chemicalList!=None):
+                                txt_file.write('<KEYWORDS_MESHCHEMICAL_LIMTOX>\n')
+                                for chemical in chemicalList.findall("Chemical"):
+                                    name_substance = chemical.find("NameOfSubstance")
+                                    if(name_substance!=None):
+                                        txt_file.write("<CHEMICAL><NAME>"+name_substance.text+"</NAME>"+"<UI>"+name_substance.get('UI')+"</UI></CHEMICAL>"+'\n')
+                                txt_file.write('</KEYWORDS_MESHCHEMICAL_LIMTOX>\n')    
+                            keyWords = article.find("MedlineCitation").find("KeywordList")        
+                            if(keyWords!=None):
+                                txt_file.write('<KEYWORDS_MANUAL_LIMTOX>')
+                                for keyword in keyWords.findall("Keyword"):
+                                    txt_file.write("<NAME>"+keyword.text+"</NAME>"+'\n')
+                                txt_file.write('</KEYWORDS_MANUAL_LIMTOX>')   
+                            meshs = article.find("MedlineCitation").find("MeshHeadingList")        
+                            if(meshs!=None):
+                                txt_file.write('<KEYWORDS_MESH_LIMTOX>\n')
+                                for mesh in meshs.findall("MeshHeading"):
+                                    name_mesh = mesh.find("DescriptorName")
+                                    if(name_mesh!=None):
+                                        txt_file.write("<NAME>"+name_mesh.text+"</NAME>"+'\n')
+                                txt_file.write('</KEYWORDS_MESH_LIMTOX>\n')        
+                            txt_file.write('</KEYWORDS_LIMTOX>\n')            
+                            txt_file.write('</ARTICLE_LIMTOX>\n')
+                            
+                            txt_file.flush()
+                            txt_file.close()    
+                            pubmedArticle.txt=1
+                            pubmedArticle.txt_path=pubMedRetrieval.unzip_path + "/" + str(internal_folder_q)
+                            pubmedArticle.txt_datetime=datetime.now()
+                            dao.save(pubmedArticle) 
+                            docs_quantity = docs_quantity - 1
+                            if(docs_quantity==0):
+                                docs_quantity=DOCS_FOR_FOLDER
+                    except Exception as inst:
+                        print "Error Generando el JSON/TXT PMID " + pmid
+                        print inst
+                        x = inst.args
+                        print x 
+
                         
                                 
