574c7d0276be64c7d84ae0a3bae63bec7f6ac7ae
javi <javicorvi@gmail.com>
2020-05-11 16:51:57 +0200

    debbie standardization

--- pubmed_standardization.py
+++ pubmed_standardization.py
@@ -10,8 +10,8 @@ import logging
 logging.basicConfig(format='%(levelname)s:%(message)s', level=logging.DEBUG)
 
 parser=argparse.ArgumentParser()
+parser.add_argument('-i', help='Input Directory')
 parser.add_argument('-o', help='Output Directory')
-parser.add_argument('-p', help='Path Parameters')
 args=parser.parse_args()
 parameters={}
 if __name__ == '__main__':
@@ -20,7 +20,7 @@ if __name__ == '__main__':
     pubmed_standardization.Main(parameters)
 
 def Main(parameters):
-    standardization_output=parameters['output']
+    standardization_output= parameters['output']
     standardization_input = parameters['input']
     if not os.path.exists(standardization_output):
         os.makedirs(standardization_output)
@@ -30,15 +30,13 @@ def Main(parameters):
 def ReadParameters(args):
     parameters_error=False
     parameters_obligation=False
-    if(args.p!=None):
-        Config = ConfigParser.ConfigParser()
-        Config.read(args.p)
-        parameters['output']=Config.get('MAIN', 'output')
-        parameters['input']=Config.get('MAIN', 'input')
-    else:
-        parameters_obligation=True
+    if(args.i!=None):
+        parameters['input']=args.i
+    elif (parameters_obligation):
+        print ("No input directory provided")
+        parameters_error=False
     if(args.o!=None):
-        parameters['output_directory']=args.o
+        parameters['output']=args.o
     elif (parameters_obligation):
         print ("No output directory provided")
         parameters_error=False
@@ -103,38 +101,24 @@ def standardization(standardization_input, standardization_output):
             if os.path.isfile(xml_file_path):
                 file_name = os.path.basename(xml_file_path)
                 with open(xml_file_path,'r') as xml_file:    
-                    txt_file_path=  standardization_output + "/" + file_name + ".txt"
-                    with codecs.open(txt_file_path,'w',encoding='utf8') as txt_file:
-                        logging.info("Parsing: " + xml_file_path)
-                        docXml = ET.parse(xml_file)
-                        for article in docXml.findall("PubmedArticle"):
-                            try:#next fix to do, hay documentos con abstract vacio que esta saliendo .... 
-                                pmid = article.find("MedlineCitation").find("PMID").text
-                                art_txt = pmid + "\t" + 'PUBMED' + "\t" + 'ABSTRACT' + "\t"
-                                article_xml = article.find("MedlineCitation").find("Article")
-                                abstract_xml = article_xml.find("Abstract")
-                                abstract = readAbstract(abstract_xml)
-                                if(abstract!=''):
-                                    title_xml=article_xml.find("ArticleTitle")
-                                    title = readTitle(title_xml)
-                                    if(title!=""):
-                                        art_txt = art_txt + remove_invalid_characters(title) + "\t" 
-                                    else:
-                                        art_txt = art_txt + " " + "\t"     
-                                    abstract_xml = article_xml.find("Abstract")
-                                    art_txt = art_txt + remove_invalid_characters(abstract) + "\n"
-                                    data=art_txt.split('\t')
-                                    if(len(data)==5):
-                                        txt_file.write(art_txt)
-                                        txt_file.flush()
-                                    else:
-                                        logging.error("Error Downloading  " + pmid + ". The document does not have a well tabulation format. The line: ")
-                                        logging.error(art_txt)
-                            except Exception as inst:
-                                logging.error("Error generation data set for classification " + pmid)
-                        txt_file.flush()                     
-                        txt_file.close()    
-                    xml_file.close()
+                    txt_file_path=  standardization_output + "/" + file_name + "/"
+                    if not os.path.exists(txt_file_path):
+                        os.makedirs(txt_file_path)
+                    logging.info("Parsing: " + xml_file_path)
+                    docXml = ET.parse(xml_file)
+                    for article in docXml.findall("PubmedArticle"):
+                        try: 
+                            pmid = article.find("MedlineCitation").find("PMID").text
+                            article_xml = article.find("MedlineCitation").find("Article")
+                            abstract_xml = article_xml.find("Abstract")
+                            abstract = readAbstract(abstract_xml)
+                            if(abstract!=''):
+                                with codecs.open(txt_file_path +  pmid + '.txt', 'w',encoding='utf8') as txt_file:
+                                    txt_file.write(remove_invalid_characters(abstract) + "\n")
+                                    txt_file.flush()
+                                    txt_file.close()    
+                        except Exception as inst:
+                            logging.error("Error generation data set for classification " + pmid)
                 logging.info("Standardization: " + os.path.basename(xml_file_path))
                 list_files_standardized.write(os.path.basename(xml_file_path)+"\n")
                 list_files_standardized.flush()
@@ -184,6 +168,11 @@ def itertext_abstract(self):
             elif tag2 not in ['CopyrightInformation']:
                 print tag2        
     else:
-        print "no text review warning"    
+        print "no text review warning" 
+        
+#abstract = []
+#for abstractText in abstractNode.findall("AbstractText"):
+#abstract.append("".join(abstractText.itertext()))
+#abstract = " ".join(abstract)           
                       
                                 
